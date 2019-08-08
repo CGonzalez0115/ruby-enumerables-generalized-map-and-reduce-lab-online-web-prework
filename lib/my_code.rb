4e043839fbfbb8b9face6e043fc376033fae9c81
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 19:22:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,8 @@
 # Your Code Here
+def 'my own map' (element1, element2)
+  yield ("A #{element1} and #{element2} my own map")
+end
+
+when "returns an array with all values made negative" do
+  'my own map'(map, [1, 2, 3, -9])
+end
